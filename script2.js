0429f7734210e0c6dd4ce6ace44d3f08a06152d3
Cesar-Felizardo <70537620+Cesar-Felizardo@users.noreply.github.com>
2022-08-11 10:49:43 -0300

    11-08-2022

---
+++ script2.js
@@ -0,0 +1,22 @@
+
+
+
+
+function selectOptions(values) {
+    
+    const status = document.querySelector(".status")
+    
+    if(values == 'PAGO') {
+        status.style.background = 'green'
+    } else if (values == 'CANCELADO') {
+        status.style.background = 'red'
+    } else if (values == 'Á PAGAR') {
+        status.style.background = 'orange'
+    } else if (values == 'SELECIONE') {
+        status.style.background = '#789192'
+    }else {
+        status.style.background = 'transparent'
+    }
+}
+
+
